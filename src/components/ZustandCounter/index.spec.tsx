aed2e3314cefba034a05c18a65ce6c16678def5f
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-02-26 18:06:18 -0600

    add `ZustandCounter` component test

---
+++ src/components/ZustandCounter/index.spec.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { useStore } from 'zustand-store';
+
+import { ZustandCounter } from './';
+
+const originalState = useStore.getState();
+
+beforeEach(() => {
+  useStore.setState(originalState);
+});
+
+test('will test <ReduxCounter />', async () => {
+  const user = userEvent.setup();
+
+  render(<ZustandCounter />);
+
+  const contentInfoElement = screen.getByRole('contentinfo');
+
+  expect(contentInfoElement).toHaveTextContent(/0/i);
+
+  const incrementButtonElement = screen.getByText(/increment/i);
+
+  await user.click(incrementButtonElement);
+
+  expect(contentInfoElement).toHaveTextContent(/1/i);
+});
+
+test('will check mock cleanups from <ZustandCounter />', async () => {
+  const user = userEvent.setup();
+
+  render(<ZustandCounter />);
+
+  const contentInfoElement = screen.getByRole('contentinfo');
+
+  expect(contentInfoElement).toHaveTextContent(/0/i);
+
+  const incrementButtonElement = screen.getByText(/increment/i);
+
+  await user.click(incrementButtonElement);
+
+  expect(contentInfoElement).toHaveTextContent(/1/i);
+});
