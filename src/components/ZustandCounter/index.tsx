bbcc531e25e032015dfe8af3c9d2b5faad75675f
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-02-26 18:05:54 -0600

    add `ZustandCounter` component

---
+++ src/components/ZustandCounter/index.tsx
@@ -0,0 +1,17 @@
+import { useStore } from 'zustand-store';
+
+export const ZustandCounter = () => {
+  const { count, increment } = useStore();
+
+  return (
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={increment}>
+          Increment
+        </button>
+
+        <span role="contentinfo">{count}</span>
+      </div>
+    </div>
+  );
+};
